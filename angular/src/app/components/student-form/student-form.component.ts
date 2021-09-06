633e7ecf95952420fd1882d694ebde87edb2664e
kss-nn <67142065+kss-nn@users.noreply.github.com>
2021-09-06 13:13:43 +0200

    1-2. feladat kész

--- angular/src/app/components/student-form/student-form.component.ts
+++ angular/src/app/components/student-form/student-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Student } from 'src/app/models/student';
+import { StudentHttpService } from 'src/app/service/student-http.service';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-student-form',
@@ -11,14 +16,24 @@ export class StudentFormComponent implements OnInit {
   @ViewChild('form') form: NgForm;
   reactForm: FormGroup;
 
+  student$: Observable<Student> = this.ar.params.pipe(
+    switchMap(params => this.studentService.getById(params.id))
+  );
 
-  constructor() { }
+  constructor(
+    private studentService: StudentHttpService,
+    private router: Router,
+    private ar: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
   }
 
-  saveStudent(){
-
+  saveStudent(ngForm: NgForm) {
+    this.studentService.update(ngForm.value, ngForm.value._id).subscribe(
+      student => this.router.navigate(['/', 'student-list']),
+      err => console.error(err)
+    )
   }
 
 }
